166badcc7597a5db70c35e1d7c06a8ba0865eb26
Siddesh Rane <siddeshrane@siddeshs-mbp.home>
2019-12-01 21:38:37 -0500

    all events changes

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+from Events import views as Events_views
 
 urlpatterns = [
+    url(r'^Event/$', Events_views.event_creation),
+    url(r'^event_creation_post/$', Events_views.event_creation_post),
     path('admin/', admin.site.urls),
 ]
