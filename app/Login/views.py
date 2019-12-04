0093d00f30d729a523ea0c9be5d1c245e538b6ff
rimashahsyr <58242271+rimashahsyr@users.noreply.github.com>
2019-12-04 02:51:01 -0500

    Error message

--- app/Login/views.py
+++ app/Login/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from Profile.models import ProfileInfo
 from Events.models import EventInfo
+from django.contrib import messages
 #from social.apps.django_app.default.models import social_auth_usersocialauth
 
 
@@ -14,13 +15,17 @@ def index(request):
 
 def login_post(request):
     InputEmail = request.POST["userEmail"]
-    #userPassword = request.POST["userPassword"]
+    userPassword = request.POST["userPassword"]
+
     try:
-        ProfileInfo_ = ProfileInfo.objects.get(inputEmail=InputEmail) 
+        ProfileInfo_ = ProfileInfo.objects.get(inputEmail=InputEmail, userPassword=userPassword)
         events = EventInfo.objects.all()
+        messages.success(request, 'SUCCESS!')
         return render(request, 'Event_Dashbord.html', {'events' : events})
+    
     except ProfileInfo.DoesNotExist: 
         print("username..")
+        messages.error(request, "Username or Password not correct.")
         return render(request, 'Login.html')
 
 
